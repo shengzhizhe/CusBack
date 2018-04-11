794177827455dd4e0586995fc55d6366d2312858
xuesemofa@126.com <xuesemofa@126.com>
2018-04-11 11:48:20 +0800

    更改窗口状态

--- src/main/java/org/cus/fx/App.java
+++ src/main/java/org/cus/fx/App.java
@@ -6,12 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static javafx.stage.StageStyle.UNDECORATED;
+
 /**
  *
  *
  */
 public class App extends Application {
-
+    /**
+     * 窗口设置https://blog.csdn.net/alanzyy/article/details/49273781
+     *
+     * @param primaryStage
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/index.fxml"));
@@ -20,7 +27,20 @@ public class App extends Application {
 //        加载外部css
         scene.getStylesheets().add(getClass().getResource("/css/index.css").toExternalForm());
         primaryStage.setScene(scene);
+//        禁止窗口缩放
         primaryStage.setResizable(false);
+//        设置窗口风格
+//        1) DECORATED——白色背景，带有最小化/最大化/关闭等有操作系统平台装饰（ 默认设置）
+//        2) UNDECORATED——白色背景，没有操作系统平台装饰
+//        3) TRANSPARENT——透明背景，没有操作系统平台装饰
+//        4) UTILITY——白色背景，只有关闭操作系统平台装饰
+//        5) UNIFIED——有操作系统平台装饰，消除装饰和内容之间的边框，内容背景和边框背景一致，（但要视平台支持），可以通过javafx.application.Platform.isSupported(javafx.application.ConditionalFeature)判断
+        primaryStage.initStyle(UNDECORATED);
+//        最小化，任务栏可见图标
+        primaryStage.setIconified(true);
+//        始终显示在其他窗口之上
+//        stage.setAlwaysOnTop(true);
+//        显示
         primaryStage.show();
     }
 
--- src/main/java/org/cus/fx/home/controller/HomeController.java
+++ src/main/java/org/cus/fx/home/controller/HomeController.java
@@ -12,6 +12,8 @@ import org.cus.fx.order.controller.OrderController;
 import org.cus.fx.spgl.controller.SpglController;
 import org.cus.fx.util.AlertUtil;
 
+import static javafx.stage.StageStyle.UNDECORATED;
+
 /**
  * @author ld
  * @name
@@ -38,6 +40,21 @@ public class HomeController {
         primaryStage.getScene().getStylesheets().add(getClass().getResource("/css/home/home.css").toExternalForm());
 //        窗口最大化
         primaryStage.setMaximized(true);
+//        以下设置不要重复设置
+        //        禁止窗口缩放
+//        primaryStage.setResizable(false);
+//        设置窗口风格
+//        1) DECORATED——白色背景，带有最小化/最大化/关闭等有操作系统平台装饰（ 默认设置）
+//        2) UNDECORATED——白色背景，没有操作系统平台装饰
+//        3) TRANSPARENT——透明背景，没有操作系统平台装饰
+//        4) UTILITY——白色背景，只有关闭操作系统平台装饰
+//        5) UNIFIED——有操作系统平台装饰，消除装饰和内容之间的边框，内容背景和边框背景一致，（但要视平台支持），可以通过javafx.application.Platform.isSupported(javafx.application.ConditionalFeature)判断
+//        primaryStage.initStyle(UNDECORATED);
+//        最小化，任务栏可见图标
+//        primaryStage.setIconified(true);
+//        始终显示在其他窗口之上
+//        stage.setAlwaysOnTop(true);
+//        显示
         primaryStage.show();
     }
 
