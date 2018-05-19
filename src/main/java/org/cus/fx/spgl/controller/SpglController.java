ea5ee79555666ddb324fa2677750f53ae6b8f024
xuesemofa <xuesemofa@126.com>
2018-05-19 21:39:30 +0800

    调整图片上传以及回显

--- src/main/java/org/cus/fx/spgl/controller/SpglController.java
+++ src/main/java/org/cus/fx/spgl/controller/SpglController.java
@@ -735,6 +735,11 @@ public class SpglController {
         try {
             FileChooser fileChooser = new FileChooser();
             fileChooser.setTitle("选择文件");
+            fileChooser.getExtensionFilters().addAll(
+//                    new FileChooser.ExtensionFilter("All Images", "*.*"),
+                    new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                    new FileChooser.ExtensionFilter("PNG", "*.png")
+            );
             File file = fileChooser.showOpenDialog(new Stage());
             if (file == null)
                 return "";
